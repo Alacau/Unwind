a94025ba10d4852c45b6d942a60bdddc136105dd
alanpcao@gmail.com <alanpcao@gmail.com>
2020-05-22 13:16:52 -0700

    Boilerplate UITableView

--- Unwind/Controllers/UserController.swift
+++ Unwind/Controllers/UserController.swift
@@ -9,12 +9,14 @@
 import UIKit
 import Firebase
 
-class UserController: UIViewController {
+private let reuseIdentifier = "UserArticleCell"
+
+class UserController: UITableViewController {
     
     // MARK: - Properties
     
     private var user: User
-    
+        
     // MARK: - Lifecycles
     
     init(user: User) {
@@ -37,11 +39,28 @@ class UserController: UIViewController {
     func configureUI() {
         view.backgroundColor = .white
         configureNavigationUI()
+        
+        tableView.register(UserArticleCell.self, forCellReuseIdentifier: reuseIdentifier)
     }
     
     func configureNavigationUI() {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationBar.largeTitleTextAttributes = [.font : UIFont(name: "Sarabun-Bold", size: 36)!]
-        navigationItem.title = "Profile" //  Will set this to the user's name
+        navigationItem.title = "Profile" //  Will set this to the user's name?
+    }
+}
+
+extension UserController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserArticleCell
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return UIView()
     }
 }
