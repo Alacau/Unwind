771b34422ba532bc8046ef45c4c2718e93eb858c
alanpcao@gmail.com <alanpcao@gmail.com>
2020-05-31 10:31:04 -0700

    Finish favorite cell UI

--- Unwind/View/FavoritesCell.swift
+++ Unwind/View/FavoritesCell.swift
@@ -13,27 +13,39 @@ class FavoritesCell: UITableViewCell {
     // MARK: - Properties
     
     private let profileImageView: UIImageView = {
-        let imageView = UIImageView()
+        let imageView = Utilities().simpleImageView(image: UIImage(named: "favorites"), cornerRadius: 44 / 2, borderWidth: 0.3)
+        imageView.contentMode = .center
+        imageView.setDimensions(width: 44, height: 44)
         return imageView
     }()
     
     private let fullnameLabel: UILabel = {
         let label = UILabel()
+        label.font = UIFont(name: "Sarabun", size: 20)
+        label.text = "Full Name"
         return label
     }()
     
     private let timestampLabel: UILabel = {
         let label = UILabel()
+        label.font = UIFont(name: "Sarabun", size: 16)
+        label.textColor = .gray
+        label.text = "05/31/2020"
         return label
     }()
     
     private let articleImageView: UIImageView = {
-        let imageView = UIImageView()
+        let imageView = Utilities().simpleImageView(image: UIImage(named: "favorites-filled"), cornerRadius: 96 / 4, borderWidth: 0.1)
+        imageView.image = UIImage(named: "favorites-filled")
+        imageView.contentMode = .scaleAspectFill
+        imageView.setDimensions(width: 108, height: 96)
         return imageView
     }()
     
     private let articleTitleLabel: UILabel = {
         let label = UILabel()
+        label.font = UIFont(name: "Sarabun-Bold", size: 20)
+        label.text = "Lorem ipsum"
         return label
     }()
     
@@ -42,21 +54,22 @@ class FavoritesCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        backgroundColor = .systemIndigo
+        backgroundColor = .white
         
         let userStack = UIStackView(arrangedSubviews: [profileImageView, fullnameLabel, timestampLabel])
         userStack.axis = .horizontal
-        userStack.spacing = 16
+        userStack.spacing = 12
         
         let articleStack = UIStackView(arrangedSubviews: [articleImageView, articleTitleLabel])
         articleStack.axis = .horizontal
         articleStack.spacing = 12
         
-        addSubview(userStack)
-        userStack.anchor(top: topAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 12, paddingLeft: 12, paddingRight: 12)
+        let stack = UIStackView(arrangedSubviews: [userStack, articleStack])
+        stack.axis = .vertical
+        stack.spacing = 12
         
-        addSubview(articleStack)
-        articleStack.anchor(top: userStack.bottomAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 8, paddingLeft: 12, paddingBottom: 12, paddingRight: 12)
+        addSubview(stack)
+        stack.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 12, paddingLeft: 12, paddingBottom: 12, paddingRight: 12)
     }
     
     required init?(coder: NSCoder) {
