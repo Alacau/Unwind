5ffa4a5b9881276d9646fdd92086eb30f3f68acc
alanpcao@gmail.com <alanpcao@gmail.com>
2020-06-12 13:53:48 -0700

    Create edit profile view controller

---
+++ Unwind/Controllers/EditProfileController.swift
@@ -0,0 +1,17 @@
+//
+//  EditProfileController.swift
+//  Unwind
+//
+//  Created by Alan Cao on 6/12/20.
+//  Copyright © 2020 Alan Cao. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+}
