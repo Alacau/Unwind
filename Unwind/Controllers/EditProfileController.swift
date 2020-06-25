df535ac5f90f3bf5a1cfec57bb0dbdcb67377039
alanpcao@gmail.com <alanpcao@gmail.com>
2020-06-25 10:06:15 -0700

    Setup view for header in section

--- Unwind/Controllers/EditProfileController.swift
+++ Unwind/Controllers/EditProfileController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class EditProfileController: UITableViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,9 +22,32 @@ class EditProfileController: UITableViewController {
         configureNavigation()
     }
     
+    // MARK: - API
+    
+    // MARK: - Selectors
+    
+    // MARK: - Helpers
+    
     func configureNavigation() {
         title = "Edit Profile"
         
         navigationController?.navigationBar.prefersLargeTitles = false
+    }
+}
+
+// MARK: - UITableViewDataSource & UITableViewDelegate
+
+extension EditProfileController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return UIView()
     }
 }
