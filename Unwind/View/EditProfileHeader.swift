3efd069fef192975bd4e54268b2b528c7af141ba
alanpcao@gmail.com <alanpcao@gmail.com>
2020-06-26 10:23:18 -0700

    Finish edit profile image UI

--- Unwind/View/EditProfileHeader.swift
+++ Unwind/View/EditProfileHeader.swift
@@ -10,10 +10,38 @@ import UIKit
 
 class EditProfileHeader: UIView {
     
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        imageView.setDimensions(width: 96, height: 96)
+        imageView.layer.cornerRadius = 96 / 2
+        imageView.layer.borderWidth = 0.5
+        imageView.layer.borderColor = UIColor.black.cgColor
+        imageView.image = UIImage(named: "landing")
+        return imageView
+    }()
+    
+    let editProfileImageButton: UIButton = {
+        let button = UIButton(type: .system)
+        let attributedString = NSAttributedString(string: "Edit Photo", attributes: [.font: UIFont(name: "Sarabun-Bold", size: 16)!])
+        button.setAttributedTitle(attributedString, for: .normal)
+        return button
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     
-        backgroundColor = .black
+        backgroundColor = .white
+        
+        let stack = UIStackView(arrangedSubviews: [profileImageView, editProfileImageButton])
+        stack.alignment = .center
+        stack.axis = .vertical
+        stack.spacing = 20
+        stack.distribution = .fillProportionally
+        
+        addSubview(stack)
+        stack.center(inView: self)
     }
     
     required init?(coder: NSCoder) {
